b410fa6236bed84d52bb446579903bcabe917c4d
dcnxx1 <alkisali1@hotmail.com>
2022-09-04 20:51:19 +0200

    added splide for image sliding

--- js/script.js
+++ js/script.js
@@ -3,66 +3,133 @@ const appcover = document.querySelectorAll('.cover-img')
 const radialButtons = document.querySelectorAll('.radial-button')
 const projectPage = document.querySelector('.project-page')
 const closePage = document.querySelector('.project-escape')
-
-
+const projectSection = document.querySelector('.project')
+const bodyBlack = document.querySelector('.body-black')
 var isProjectPage = false
 var currentPage = 'button1'
 // gsap.to('.cover-qr', {scale: ".5", transform: "translate(-50px, 0px)", opacity: 0, duration: .2})
 
 
-document.body.style.overflow = isProjectPage ? "hidden" : "initial"
 
+
+
+new Splide('.splide').mount()
 
 closePage.addEventListener('click', e => {
     
     gsap.fromTo(projectPage, {
         transform: "translate(0px, 0%)",
     }, {
-        transform: "translate(0px, -100%)", 
+        transform: "translate(0px, -100%)",
+        display: "none", 
         onComplete() {
             document.body.style.overflow = "initial"
         }
+    })
+    gsap.to(bodyBlack, {
+        display: 'none'
     })
    
 })
 
 radialButtons.forEach((button) => {
+    const attr = button.getAttribute('data-button')
     button.addEventListener('click', e => {
-        const attr = button.getAttribute('data-button')
-        handleCovers(attr)
-       
+        currentPage = attr
+        lightButtons(attr)
+        handleRadialButtons()
     })
 })
+
+function lightButtons(attr){
+    
+    radialButtons.forEach(button => {
+        const btnAttribute = button.getAttribute('data-button')
+        
+        if(btnAttribute == attr){
+            gsap.to(button, {
+                background: '#70e000',
+                border: '2px solid #70e000 ',
+                color: '#002855',
+                duration: .1
+            })
+        } else {
+            gsap.to(button, {
+                background: '#002855',
+                color: '#38B000',
+                border: '2px solid #38B000 ',
+                duration: .1
+            })
+        }
+    })
+}
+
+function handleOverBody () {
+    gsap.to(bodyBlack, {
+       display: 'block',
+       duration: .6
+    })
+    
+    bodyBlack.addEventListener('click', e => {
+        gsap.to(bodyBlack, {
+            display: 'none',
+            onComplete() {
+                document.body.style.overflow = "initial"
+            }
+        })
+        gsap.to(projectPage, {
+            transform: 'translate(0px, -100%)',
+            display : 'none'
+        })
+    })
+    
+}
 
 coverMore.addEventListener('click', e => {
     document.body.style.overflow = "hidden"
+    handleOverBody()
     gsap.to(projectPage, {
         display: 'flex',
-        transform: "translate(0px, 0%)"
+        transform: "translate(0px, 0%)",
+        top: `${window.scrollY}px`,
+
+        height: `80vh`,
     })
 })
 
-
-function handleCovers(buttonId){
-    switch(buttonId){
-        case 'button1':
-            
-           
-            break;
-
-        case 'button2':
-                
-            
-        break;
-
-        case 'button3':
-            
-              
-        break;
-
-        default : 
-
-            break;
-    }
-
+function handleProjectChange(imgSrc, textSrc){
+    
 }
+
+
+function handleRadialButtons() {
+    switch(currentPage){
+        case 'button1':
+        break;
+        case 'button2':
+        break;
+        case 'button3':
+        break;
+        default:
+             
+        break;
+    }
+}
+
+
+// btn inactive
+// gsap.to(button, {
+//     background: '#002855',
+//     color: '#38B000',
+//     border: '2px solid #38B000 ',
+//     duration: .1
+// })
+
+// btn active
+// gsap.to(button, {
+//     background: '#70e000',
+//     border: '2px solid #70e000 ',
+//     color: '#002855',
+//     duration: .1
+    
+// })
