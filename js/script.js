7d2a09bdb3537af97a8393a48f86ce46f403bfb8
dcnxx1 <alkisali1@hotmail.com>
2022-09-09 03:20:15 +0200

    finished contact page moving onto cv

--- js/script.js
+++ js/script.js
@@ -38,17 +38,17 @@ splide.mount()
 
 nextButton.addEventListener('click', e => {
     splide.go('>')
-   removeButtons(splide.length, splide.index)
+    removeButtons(splide.length, splide.index)
 })
 
-function removeButtons(length, index){
-    if(index + 1 === length){
-        nextButton.style.visibility  = "hidden"
+function removeButtons(length, index) {
+    if (index + 1 === length) {
+        nextButton.style.visibility = "hidden"
     } else {
         nextButton.style.visibility = "visible"
     }
 
-    if(index === 0) {
+    if (index === 0) {
         gsap.to(previousButton, {
             visibility: 'hidden',
             duration: 1,
@@ -63,37 +63,37 @@ function removeButtons(length, index){
     }
 }
 
-previousButton.addEventListener('click',e => {
+previousButton.addEventListener('click', e => {
     splide.go('<')
     removeButtons(splide.length, splide.index)
 })
 
-function pageTo(attr){
-        
-        if(attr === "alktunes"){
-            splide.go(0)
-            removeButtons(splide.length, splide.index)
-        }
-    
-        if(attr === "shopee"){
-            splide.go(1)
-            removeButtons(splide.length, splide.index)
-        }
-    
-        if(attr === "qrcode"){
-            splide.go(2)
-            removeButtons(splide.length, splide.index)
-        }
-    
-    
+function pageTo(attr) {
+
+    if (attr === "alktunes") {
+        splide.go(0)
+        removeButtons(splide.length, splide.index)
+    }
+
+    if (attr === "shopee") {
+        splide.go(1)
+        removeButtons(splide.length, splide.index)
+    }
+
+    if (attr === "qrcode") {
+        splide.go(2)
+        removeButtons(splide.length, splide.index)
+    }
+
+
 }
 
 covers.forEach(cover => {
     cover.addEventListener('click', e => {
         document.body.style.overflow = "hidden"
         const attr = cover.getAttribute('data-cover')
-        
-  
+
+
         handleOverBody()
         handleNavDown()
         gsap.fromTo(projectPage, {
@@ -104,7 +104,7 @@ covers.forEach(cover => {
             transform: "translate(0px, 0%)",
             top: `${window.scrollY}px`,
             height: `80vh`,
-            onComplete () {
+            onComplete() {
                 pageTo(attr)
             }
         })
@@ -112,21 +112,21 @@ covers.forEach(cover => {
 })
 
 closePage.addEventListener('click', e => {
-    
+
 
     gsap.to(projectPage, {
         transform: `translate(0px, -100%)`,
         display: "none",
-      
+
         onComplete() {
             document.body.style.overflow = "initial"
-            
+
         }
     })
     gsap.to(bodyBlack, {
         display: 'none'
     })
-   
+
 })
 
 radialButtons.forEach((button) => {
@@ -138,12 +138,12 @@ radialButtons.forEach((button) => {
     })
 })
 
-function lightButtons(attr){
-    
+function lightButtons(attr) {
+
     radialButtons.forEach(button => {
         const btnAttribute = button.getAttribute('data-button')
-        
-        if(btnAttribute == attr){
+
+        if (btnAttribute == attr) {
             gsap.to(button, {
                 background: '#70e000',
                 border: '2px solid #70e000 ',
@@ -161,12 +161,12 @@ function lightButtons(attr){
     })
 }
 
-function handleOverBody () {
+function handleOverBody() {
     gsap.to(bodyBlack, {
-       display: 'block',
-       duration: .6
+        display: 'block',
+        duration: .6
     })
-    
+
 }
 
 
@@ -176,20 +176,27 @@ function handleOverBody () {
 const navLinks = document.querySelectorAll('.nav-links')
 
 document.querySelector('.logo-name').addEventListener('click', e => {
-    gsap.to(window, {duration : 1.3, scrollTo: `#intro`, ease: "power2"})
+    gsap.to(window, { duration: 1.3, scrollTo: `#intro`, ease: "power2" })
 })
+
 navLinks.forEach(link => {
     link.addEventListener('click', e => {
         e.preventDefault()
         const attr = link.getAttribute('data-link')
-        gsap.to(window, {duration: 1.3, scrollTo: {y: `#${attr}`, offsetY: getWidth(attr)}, ease: "power2"})
+        if (attr === 'contact') {
+            revealContact()
+        } else if (attr === 'cv') {
+
+        } else {
+            gsap.to(window, { duration: 1.3, scrollTo: { y: `#${attr}`, offsetY: getWidth(attr) }, ease: "power2" })
+        }
     })
 
 })
 
 var oldScroll = window.scrollY
 
-function closeBody(){
+function closeBody() {
     gsap.to(bodyBlack, {
         display: 'none'
     })
@@ -198,50 +205,46 @@ function closeBody(){
 
 
 window.onscroll = (e) => {
-
-
     var currentScrollPosition = window.scrollY
-    console.log(window.scrollY)
 
-    if(currentScrollPosition <= 91){
+    if (currentScrollPosition <= 91) {
         navNormal()
-    } 
+    }
 
-
-    if(window.scrollY >= 91){
-        if(oldScroll > currentScrollPosition){
+    if (window.scrollY >= 91) {
+        if (oldScroll > currentScrollPosition) {
             // scroll up 
             handleNavUp()
-           } else {
-               // scroll down
+        } else {
+            // scroll down
             handleNavDown()
         }
     }
-    
+
     oldScroll = currentScrollPosition
 }
 
 function navNormal() {
     gsap.to('nav', {
-       y: 0,
-       left: '0%',
-       transform: 'translateX(0%)',
-       width: '100%',
-       position: 'fixed',
-       background: 'initial',
-       borderRadius: '0px',
-    }) 
+        y: 0,
+        left: '0%',
+        transform: 'translateX(0%)',
+        width: '100%',
+        position: 'fixed',
+        background: 'initial',
+        borderRadius: '0px',
+    })
 }
 
 function handleNavUp() {
     gsap.to('nav', {
-       y: 20,
-       width: '80%',
-       position: 'fixed',
-       background: '#001845',
-       left: '50%',
-       transform: 'translateX(-50%)',
-       borderRadius: '20px',
+        y: 20,
+        width: '80%',
+        position: 'fixed',
+        background: '#001845',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        borderRadius: '20px',
     })
 
 }
@@ -251,75 +254,73 @@ function handleNavDown() {
         y: -140,
         transform: 'translateX(-50%)',
         left: '50%',
-       
+
     })
 
-   
+
 }
 
-function closeMenu(){
+function closeMenu() {
     gsap.to('.menu-mobile', {
         top: `${window.scrollY}px`,
         display: 'none',
         transform: 'translate(100%)',
-        ease: "power2",  
+        ease: "power2",
     })
 }
 
 bodyBlack.addEventListener('click', e => {
-    
+
     closeMenu()
-    if(window.scrollY > 91){
+    if (window.scrollY > 91) {
         handleNavUp()
-       
+
     }
 
 
     gsap.to(projectPage, {
         transform: 'translate(0px, -100%)',
-        display : 'none'
+        display: 'none'
     })
     document.body.style.overflow = 'scroll'
     gsap.to(bodyBlack, {
         display: 'none'
     })
+    closeContact()
 })
 
 
 
 window.onresize = e => {
-    if(window.innerWidth > 770){
-        
-       
+    if (window.innerWidth > 770) {
+
         gsap.fromTo(projectPage, {
             transform: 'translate(0%, 0%)',
             top: `${window.scrollY}px`,
         }, {
             transform: 'translate(0px, -100%)',
             top: `${window.scrollY}px`,
-            display: 'none'
+            display: 'none',
+            onComplete () {
+                closeBody()
+            }
         })
         gsap.to('.menu-mobile', {
             display: 'none',
             transform: 'translateX(100%)',
-            
-        })
-
-        gsap.to(bodyBlack, {
-            display: 'none',
             onComplete () {
-                document.body.style.overflow = "initial"
+               closeBody()
             }
         })
-        
+
     }
 }
 
-function getWidth (attr) {
-    if(window.innerWidth <= 700){
+function getWidth(attr) {
+    if (window.innerWidth <= 700) {
         return 65
     } else {
-        if(attr == "about"){
+        if (attr == "about") {
             return 95
         } else {
             return 80
@@ -332,14 +333,20 @@ function getWidth (attr) {
 
 const menuMobile = document.querySelector('.nav-menu')
 const mobileLinks = document.querySelectorAll('.mobile-link')
-
+// cv = resume
 mobileLinks.forEach(link => {
     link.addEventListener('click', e => {
         const attr = link.getAttribute('data-mobile-link')
-        gsap.to(window, {duration: 1.3, scrollTo: {y: `#${attr}`, offsetY: getWidth(attr)}, ease: "power2"})
-        closeBody()
-        closeMenu()
-        handleNavDown()
+        if (attr === 'contact') {
+            revealContact()
+        } else if (attr === 'cv') {
+            alert("something else")
+        } else {
+            gsap.to(window, { duration: 1.3, scrollTo: { y: `#${attr}`, offsetY: getWidth(attr) }, ease: "power2" })
+            closeBody()
+            closeMenu()
+            handleNavDown()
+        }
     })
 })
 
@@ -349,39 +356,38 @@ menuMobile.addEventListener('click', e => {
 
 function showMenuMobile() {
     document.body.style.overflow = "hidden"
-    
-    if(window.scrollY >91){
+
+    if (window.scrollY > 91) {
         handleNavDown()
     }
 
     gsap.to('.menu-mobile', {
         top: `${window.scrollY}`,
         duration: 0,
-        onComplete () {
+        onComplete() {
             gsap.to('.menu-mobile', {
                 display: 'flex',
                 transform: 'translate(0%)',
                 ease: "power2",
             })
         }
-        
-        
+
+
     })
-    
+
     gsap.to(bodyBlack, {
         display: 'block'
     })
 
     bodyBlack.addEventListener('click', e => {
-        // handleNavUp()
-        
+
         gsap.to(bodyBlack, {
             display: 'none',
-            onComplete () {
+            onComplete() {
                 document.body.style.overflow = "scroll"
             }
         })
-       
+
 
     })
 }
@@ -389,8 +395,8 @@ function showMenuMobile() {
 
 // EFFECTS
 
-const introTimeline  = gsap.timeline();
-introTimeline.to('.fader-right', {transform: 'translat(0%, 0%)', opacity: 1, stagger: 1, duration : .1})
+const introTimeline = gsap.timeline();
+introTimeline.to('.fader-right', { transform: 'translat(0%, 0%)', opacity: 1, stagger: 1, duration: .1 })
 
 const aboutTimeLine = gsap.timeline({
     scrollTrigger: {
@@ -399,13 +405,13 @@ const aboutTimeLine = gsap.timeline({
     }
 })
 
-aboutTimeLine.to('.about-text-top', {opacity: 1})
-.to('.about-container-stack', {opacity: 1, transform: 'translateX(0%)'})
-.to('.about-card-show', {opacity: 1, transform: 'translateX(0%)' , stagger: .1})
+aboutTimeLine.to('.about-text-top', { opacity: 1 })
+    .to('.about-container-stack', { opacity: 1, transform: 'translateX(0%)' })
+    .to('.about-card-show', { opacity: 1, transform: 'translateX(0%)', stagger: .1 })
 
 const projectTimeLine = gsap.timeline({
     scrollTrigger: {
-        trigger:  '.project-container',
+        trigger: '.project-container',
         start: 'top center',
     }
 })
@@ -415,7 +421,7 @@ projectTimeLine.to('.proj-cover', {
     transform: 'translateX(0%)',
     duration: 1
 })
-.to('.proj-link', { opacity: 1,})
+    .to('.proj-link', { opacity: 1, })
 
 const imgTimeline = gsap.timeline({
     scrollTrigger: {
@@ -423,17 +429,17 @@ const imgTimeline = gsap.timeline({
         start: 'top-center',
     }
 })
-.to('.img-cover', {
-    opacity: 1,
-})
-.to('.cover-link', {
-    opacity: 1,
-    transform: 'translateY(0%)',
-    stagger: .2,
-    duration: .1
-})
+    .to('.img-cover', {
+        opacity: 1,
+    })
+    .to('.cover-link', {
+        opacity: 1,
+        transform: 'translateY(0%)',
+        stagger: .2,
+        duration: .1
+    })
 
-.to('.anim-cover-text', { opacity: 1, })
+    .to('.anim-cover-text', { opacity: 1, })
 const activityTimeline = gsap.timeline({
     scrollTrigger: {
         trigger: '.show-activity',
@@ -445,11 +451,74 @@ activityTimeline.to('.activity-show-cover', {
     opacity: 1,
     transform: 'translateX(0%)',
 })
-.to('.activity-img', {
-    opacity: 1,
-    transform: 'translateX(0%)'
-})
-.to('.activity-txt', {
-    opacity: 1,
-    transform: 'translateY(0%)'
-})
+    .to('.activity-img', {
+        opacity: 1,
+        transform: 'translateX(0%)'
+    })
+    .to('.activity-txt', {
+        opacity: 1,
+        transform: 'translateY(0%)'
+    })
+
+const closeContactBtn = document.querySelector('.close-contact')
+
+function revealContact() {
+    document.body.style.overflow = 'hidden'
+    const revealTimeLine = gsap.timeline()
+    closeMenu()
+    handleNavDown()
+    revealTimeLine.to('.cta-contact', {
+        width: '90%',
+        height: '50%',
+        position: 'fixed',
+        top: `${window.screenTop}px`,
+        display: 'flex',
+        opacity: 1,
+    })
+    .to('.contact-header', {
+        opacity: 1,
+    })
+    .to(bodyBlack, {
+        display: 'block'
+    })
+    .to('.contact-item', {
+        opacity : 1,
+    })
+    .to('.contact-message', {
+        opacity: 1,
+    })
+
+    bodyBlack.addEventListener('click', e => {
+        closeContact()
+    })
+
+    closeContactBtn.addEventListener('click', e => {
+        closeContact()
+    })
+
+}
+
+function closeContact() {
+    const closeTimeline = gsap.timeline()
+
+    closeTimeline.to('.contact-header', {
+        opacity: 0,
+    })
+    .to('.contact-item', {
+        opacity: 0
+    })
+    .to('.contact-message', {
+        opacity: 0,
+    })
+    .to('.cta-contact', {
+        display: 'none',
+        top: `${window.screenTop}px`,
+        opacity: 0,
+        width: '0%',
+        height: '0%'
+    })
+    
+    closeBody()
+    handleNavUp()
+
+}
